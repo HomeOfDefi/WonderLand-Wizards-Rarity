692a93c9169733859109e91048f42abd27b2c5a9
HomeOfDefi <97765551+HomeOfDefi@users.noreply.github.com>
2022-08-02 12:56:53 +0100

    Update config.js

--- WonderLand Wizards Rarity/config/config.js
+++ WonderLand Wizards Rarity/config/config.js
@@ -1,10 +1,10 @@
 const config = {
-    app_name: 'WonderLand Wizzards',
-    app_description: 'WonderLand Wizzards is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
+    app_name: 'Wonderland Wizards',
+    app_description: 'WonderLand Wizards is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
     collection_file_name: '_metadata.json',
     collection_contract_address: '0x8bef954862d333fbcceadd66048565db283c96ff',
     collection_name: '5k Wonderland Wizzards',
-    collection_description: '5k "One Day I\'ll Be A Punk"-punks – a homage to the one and only Wonderland Wizzards. Holding a OneDayPunk will give you early access to PunkScapes and reserve a profile on the PunkScape website.',
+    collection_description: 'WonderlandWizards is a collection of 1000 wizards on the Ethereum blockchain designed to bring holders rewards weekly',
     collection_id_from: 1,
     ignore_traits: ['date'], 
     sqlite_file_name: 'database_wonderland_wizzards.sqlite',
